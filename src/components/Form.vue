5b79ba50262554a625bc1d0c214410f1bed3a6e6
Thomas Nabord <thomas.nabord@prestashop.com>
2019-10-15 14:16:17 +0100

    Add switch options

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,13 +5,37 @@
 
     <form @submit="saveFlat">
       <div class="form-group row">
-        <label for="flatFormLink" class="col-sm-2 col-form-label">Link</label>
+        <label for="flatFormLink" class="col-sm-2 col-form-label">Lien annonce</label>
         <div class="col-sm-10">
           <input v-model="form.Link" type="text" class="form-control"
-            id="flatFormLink" placeholder="Link to the ad"
+            id="flatFormLink" placeholder="http://"
           >
         </div>
       </div>
+      <h2>Chauffage</h2>
+      <div class="form-group row">
+        <label for="customSwitch1" class="col-sm-3 col-form-label">Chauffage centralise</label>
+        <div class="col-sm-9 custom-control custom-switch">
+          <input v-model="form.chauffage_centralise" type="checkbox" class="custom-control-input" id="customSwitch1">
+          <label class="custom-control-label" for="customSwitch1" ></label>
+        </div>
+      </div>
+      <h2>Rangements</h2>
+      <div class="form-group row">
+        <label for="switchStorageEnough" class="col-sm-3 col-form-label">Espaces de rangement suffisants</label>
+        <div class="col-sm-9 custom-control custom-switch">
+          <input v-model="form.storage_enough" type="checkbox" class="custom-control-input" id="switchStorageEnough">
+          <label class="custom-control-label" for="switchStorageEnough" ></label>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="switchStoragePossible" class="col-sm-3 col-form-label">Espace dispo pour rangements</label>
+        <div class="col-sm-9 custom-control custom-switch">
+          <input v-model="form.storage_possible" type="checkbox" class="custom-control-input" id="switchStoragePossible">
+          <label class="custom-control-label" for="switchStoragePossible" ></label>
+        </div>
+      </div>
+
       <button class="btn btn-primary" type="submit">Save</button>
     </form>
 
