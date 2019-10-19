c63c5669f477799acda0d807d7b31e8234d0d4a7
Thomas Nabord <thomas.nabord@prestashop.com>
2019-10-19 23:07:02 +0100

    Add hoodmaps to the list

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -2,9 +2,19 @@
   <div>
     <h2>Links</h2>
 
-    <a href="https://www.london.gov.uk/what-we-do/housing-and-land/improving-private-rented-sector/london-rents-map">
-      Rents map of London
-    </a>
+    <ul>
+      <li>
+        <a href="https://www.london.gov.uk/what-we-do/housing-and-land/improving-private-rented-sector/london-rents-map">
+          Rents map of London
+        </a>
+      </li>
+
+      <li>
+        <a href="https://hoodmaps.com/london-neighborhood-map">
+          Community Map
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
